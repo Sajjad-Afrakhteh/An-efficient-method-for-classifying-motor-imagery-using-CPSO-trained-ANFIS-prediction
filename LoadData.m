3a6667e71949b2d50cd725ea9bb4a7d78c8aa0e4
Sajjad Afrakhteh <33925742+sajjad1023@users.noreply.github.com>
2019-03-06 16:56:33 +0330

    Update LoadData.m

--- LoadData.m
+++ LoadData.m
@@ -1,16 +1,3 @@
-%
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-%
-% Project Code: YPFZ104
-% Project Title: Evolutionary ANFIS Traing in MATLAB
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
-
 function data=LoadData()
 
     data=load('engine_data');
